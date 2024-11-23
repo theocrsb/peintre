e587e4ec7ed3bf5466940ca8dc3010aa898d9992
theocrsb <theo_crosbie@yahoo.fr>
2024-11-23 17:35:21 +0100

    update footer
    Signed-off-by: theocrsb <theo_crosbie@yahoo.fr>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <footer
@@ -6,22 +8,9 @@ const Footer = () => {
     >
       <div className="text-center p-3">
         <p>© {new Date().getFullYear()} HAJAR DECORS. Tous droits réservés.</p>
-        <a
-          className="text-body mx-2"
-          href="https://instagram.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Instagram
-        </a>
-        <a
-          href="https://facebook.com mx-2"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-body"
-        >
-          Facebook
-        </a>
+        <Link className="nav-link" to="/contact">
+          Contact
+        </Link>
       </div>
     </footer>
   );
